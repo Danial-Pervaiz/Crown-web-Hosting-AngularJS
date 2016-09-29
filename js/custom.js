31d71f85a1c6b0ee029c6af5e3e34aa2cbb76b1f
Danial Pervaiz <codemythought@gmail.com>
2016-09-28 20:15:29 -0600

    Added Changes

--- js/custom.js
+++ js/custom.js
@@ -6,15 +6,25 @@ $('.language').click(function(){
 	$('.languageDropdown').fadeToggle();
 });
 $('.languageDropdown li').on('click', function(){
-	if( $(this).text() === 'ENGLISH'){
-		alert('english');
+	var reg = /^\s*|\s*$/g;  
+	var text = $(this).text().replace(reg, "");
+	if( text === 'ENGLISH'){
+		$('.language a').text('ENG');
+		$('.languageDropdown').fadeOut();
 	}
-	else{
-		alert('wrong value');
+	if( text === 'SPANISH'){
+		$('.language a').text('SPN');
+		$('.languageDropdown').fadeOut();
 	}
-	
+	if( text === 'ARABIC'){
+		$('.language a').text('ARB');
+		$('.languageDropdown').fadeOut();
+	}
+	if( text === 'TELUGU'){
+		$('.language a').text('TLG');
+		$('.languageDropdown').fadeOut();
+	}
 });
-
 
 
 //Please change this to complete the language section
