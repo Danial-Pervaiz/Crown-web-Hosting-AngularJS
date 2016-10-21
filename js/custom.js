3679e146e0bf64363e366baec0e608fb39945236
Danial Pervaiz <codemythought@gmail.com>
2016-10-20 21:00:02 -0600

    activator

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,7 @@
 
 // switcher for the header section. when the user clicks on the current item, it will activate the current item
 $('#activator li ').on('click', function(){
-	$(this).css({
-		'background-color':'#1e88e5'
-	}).siblings().css({
-		'background-color':'#0096ff'
-	});
+	$(this).addClass('bazigar').siblings().removeClass('bazigar');
 });
 
 $('#activator li a').on('click', function(){
